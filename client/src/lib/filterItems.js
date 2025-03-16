74ebe3d3ffadcbc1a87305ee1067698a300c913d
sreyas <sreyas1045@gmail.com>
2025-03-16 16:11:54 +0530

    some changes

--- client/src/lib/filterItems.js
+++ client/src/lib/filterItems.js
@@ -1,7 +1,7 @@
 export const filterItems = (items, query) => {
-    const filteredItems = items?.filter((item) =>
-      item.title.toLowerCase().includes(query.toLowerCase())
-    );
-    console.log(filteredItems);
-    return filteredItems;
-  };
+  const filteredItems = items?.filter((item) =>
+    item.title.toLowerCase().includes(query.toLowerCase())
+  );
+  console.log(filteredItems);
+  return filteredItems;
+};
