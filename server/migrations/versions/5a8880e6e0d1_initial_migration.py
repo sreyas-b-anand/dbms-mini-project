a06b59be69f7eb0ce0f238d0d7bf3e10ffa47750
sreyas <sreyas1045@gmail.com>
2025-07-08 18:07:43 +0530

    it works

--- server/migrations/versions/5a8880e6e0d1_initial_migration.py
+++ server/migrations/versions/5a8880e6e0d1_initial_migration.py
@@ -1,8 +1,8 @@
-"""empty message
+"""Initial migration
 
-Revision ID: d9047ed67891
+Revision ID: 5a8880e6e0d1
 Revises: 
-Create Date: 2025-04-06 22:51:10.146294
+Create Date: 2025-07-08 16:40:31.247793
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'd9047ed67891'
+revision = '5a8880e6e0d1'
 down_revision = None
 branch_labels = None
 depends_on = None
